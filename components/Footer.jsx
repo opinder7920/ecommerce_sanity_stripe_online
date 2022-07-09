7122b946fbf47f4e94c884e80a8bce58fc02df2a
opinder pal singh <opinder79@gmail.com>
2022-07-09 23:26:39 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>&copy;2022  * KLICKMEDIAUK  * All rights reserved</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  )
+}
+
+export default Footer;
